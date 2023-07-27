f9c886b7ab4946952475e08381beb1b77eec8440
iqbal_agyan <iqbal.agyan@gmail.com>
2023-07-27 10:46:00 +0700

    update

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -1,6 +1,9 @@
 <?php 
 $data['warna'] = 'transparent';
 $this->load->view('navbar',$data); ?>
-<div class="container-fluid vh-100 p-0" style="position: absolute;top: 0;">
+<div class="container-fluid vh-100 p-0">
+  <img src="<?= base_url() ?>assets/img/app/home.png" style="width: 100%;">
+</div>
+<div class="container-fluid vh-100 p-0">
   <img src="<?= base_url() ?>assets/img/app/background.png" style="width: 100%;">
 </div>
